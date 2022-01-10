ea6da8ef15a912794ebbce4f8514c3c54174be5c
Binh An <ngbinhan1512@gmail.com>
2022-01-10 14:36:15 +0700

    update

---
+++ ADKT_AndroidProjectBackend/UpdateModel/StudentUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADKT_AndroidProjectBackend.UpdateModel
+{
+    public class StudentUpdateModel
+    {
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public string Name { set; get; }
+        public string BirthDay { set; get; }
+        public string Email { set; get; }
+    }
+}
